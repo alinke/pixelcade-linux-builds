9910c87d073050f8b2e5b77548aac459d748910c
Alfred Linke <al@ledpixelart.com>
2023-01-03 00:27:53 -0800

    autostart.sh for the new pixelweb , this is only used on a device that does not have Batocera (custom.sh) or RetroPie (autostart.sh)

--- system/autostart.sh
+++ system/autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# startup script for a dedicated Pi for Pixelcade, systemd calls this via /etc/systemd/system/pixelcade.service
+# startup script for a dedicated Pi for Pixelcade that is not running RetroPie, systemd calls this via /etc/systemd/system/pixelcade.service
 PIXELHOME=$HOME/pixelcade
-cd $PIXELHOME && ./pixelweb &
+cd $PIXELHOME && ./pixelweb -image "system/retropie.png" -startup &
