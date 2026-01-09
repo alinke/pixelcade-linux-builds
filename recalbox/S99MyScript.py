242724774318c65d387c4a90e1cf51946579471b
Al Linke <al@pixelcade.org>
2026-01-09 00:53:55 -0800

    removed the 5s delay hack

--- recalbox/S99MyScript.py
+++ recalbox/S99MyScript.py
@@ -2,7 +2,6 @@
 
 case "$1" in
    start)
-         sleep 5 #work around as for some reason if pixelweb starts too early, it gets killed
          cd /etc/init.d/pixelcade && ./pixelweb -p /recalbox/share/pixelcade-art/ -port 7070 -image "system/recalbox.png" -startup &
          ;;
    stop)
