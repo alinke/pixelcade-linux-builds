d27b6b580878a964cd8ce51ec5eba72ea73c297d
Al Linke <al@pixelcade.org>
2026-05-15 19:32:14 -0700

    attract mode plug in

---
+++ attractmode-plugin/Pixelcade/scripts/update_pixelcade.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+display_text="$1"
+emulator="$2"
+rom="$3"
+emulator=$(sed 's/ /%20/g' <<< "$emulator")
+rom=$(sed 's/ /%20/g' <<< "$rom")
+if [ "$emulator" = "@" ] ; then
+  curl -s -o /dev/null -G --data-urlencode "t=$display_text" "http://localhost:8080/console/stream/$rom"
+else
+  curl -s -o /dev/null -G --data-urlencode "t=$display_text" "http://localhost:8080/arcade/stream/$emulator/$rom"
+fi
