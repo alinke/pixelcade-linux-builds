19d0260b46cdcd111d82983634de87f70abfe820
Al Linke <al@pixelcade.org>
2026-07-04 22:28:49 -0700

    removing black call

--- rcade/scripts/game-start/pixelcade.sh
+++ rcade/scripts/game-start/pixelcade.sh
@@ -112,9 +112,9 @@ havehighscore() {
 # Main Code Start Here
 	if [ "$SYSTEM" != "" ] && [ "$GAMENAME" != "" ]; then
   	  #clear the Pixelcade Queue, see http://pixelcade.org/api for info on the Queue feature
-  		PIXELCADEURL="console/stream/black"
-  		curl -s "$PIXELCADEBASEURL$PIXELCADEURL" >> /dev/null 2>/dev/null & #this was causing an issue on new pixelweb
-			sleep 1 #TO DO for some reason, doesn't always work without this, in theory it should not be needed
+  		#PIXELCADEURL="console/stream/black"
+  		#curl -s "$PIXELCADEBASEURL$PIXELCADEURL" >> /dev/null 2>/dev/null & #this was causing an issue on new pixelweb
+			#sleep 1 #TO DO for some reason, doesn't always work without this, in theory it should not be needed
 			URLENCODED_GAMENAME=$(rawurlencode "$GAMENAME")
       URLENCODED_TITLE=$(rawurlencode "$GAMETITLE")
       #let's make a call here if this game has high scores
