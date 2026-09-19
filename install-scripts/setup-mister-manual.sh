c8604cd0a72509b85bde35c39f80ccc07b77c062
Al Linke <al@pixelcade.org>
2026-09-19 12:26:15 -0700

    manual script version

---
+++ install-scripts/setup-mister-manual.sh
@@ -0,0 +1,326 @@
+#!/bin/bash
+stretch_os=false
+buster_os=false
+ubuntu_os=false
+retropie=false
+pizero=false
+pi4=false
+java_installed=false
+install_succesful=false
+auto_update=false
+attractmode=false
+beta=false
+black=`tput setaf 0`
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+white=`tput setaf 7`
+reset=`tput sgr0`
+machine_arch=default
+version=12  #increment this as the script is updated
+#echo "${red}red text ${green}green text${reset}"
+
+# Parse command line arguments
+for arg in "$@"; do
+    case $arg in
+        beta|--beta|-beta)
+            beta=true
+            ;;
+    esac
+done
+
+cat << "EOF"
+       _          _               _
+ _ __ (_)_  _____| | ___ __ _  __| | ___
+| '_ \| \ \/ / _ \ |/ __/ _` |/ _` |/ _ \
+| |_) | |>  <  __/ | (_| (_| | (_| |  __/
+| .__/|_/_/\_\___|_|\___\__,_|\__,_|\___|
+|_|
+EOF
+
+#check Space
+#check if user_launch is there, if so then add runpixelcade.sh there,if not then tell user to upgrade mister first
+#check if mister example is there or mister.ini and modify
+# MiSTer_example.ini
+#_user-startup.sh
+
+echo "${magenta}       Pixelcade for MiSTer : Installer Version $version    ${white}"
+echo "${magenta}       *** MANUAL START MODE - Pixelcade will NOT auto-start on boot ***${white}"
+if [[ "$beta" == "true" ]]; then
+    echo "${magenta}       *** BETA MODE ENABLED ***${white}"
+fi
+echo ""
+echo "Now connect Pixelcade to a free USB port on your MiSTer"
+echo "If you have a Pixelcade LCD Marquee, ensure it is connected to your WiFi or Ethernet network and cannot be connected via USB to MiSTer"
+echo "Grab some coffee or tea, this installer will take around 30 minutes to complete"
+
+INSTALLPATH="/media/fat/"
+
+function pause(){
+ read -s -n 1 -p "Press any key to continue . . ."
+ echo ""
+}
+
+add_log_entry_if_needed() {
+    echo "${yellow}Ensuring only one log_file_entry=1 exists in MiSTer.ini...${white}"
+    
+    # Create a temporary file
+    temp_file=$(mktemp)
+    
+    # Remove all existing log_file_entry lines and copy the rest to temp file
+    grep -v "^log_file_entry=" MiSTer.ini > "$temp_file"
+    
+    # Check if [MiSTer] section exists
+    if grep -q "^\[MiSTer\]" "$temp_file"; then
+        # Add log_file_entry=1 after [MiSTer] section
+        sed -i '/^\[MiSTer\]/a\log_file_entry=1' "$temp_file"
+        echo "${yellow}Added log_file_entry=1 after [MiSTer] section${white}"
+    else
+        # If no [MiSTer] section, add it at the beginning with log_file_entry=1
+        echo -e "[MiSTer]\nlog_file_entry=1\n$(cat "$temp_file")" > "$temp_file"
+        echo "${yellow}Added [MiSTer] section with log_file_entry=1${white}"
+    fi
+    
+    # Replace the original file with the cleaned up version
+    mv "$temp_file" MiSTer.ini
+    
+    echo "${yellow}Successfully ensured single log_file_entry=1 in MiSTer.ini${white}"
+}
+
+echo "Stopping Pixelcade (if running...)"
+# let's make sure pixelweb is not already running
+killall java #in the case user has java pixelweb running
+curl localhost:8080/quit
+
+#do we have enough disk space?
+cd ${INSTALLPATH}
+FREE=`df -k --output=avail "$PWD" | tail -n1`   # df -k not df -h
+if [[ $FREE -lt 1048576 ]]; then               # 1G = 10*1024*1024k
+  echo "${yellow}Sorry, you'll need at least 1 GB of free disk space on ${INSTALLPATH}${white}"
+  exit 1
+fi
+
+#do we have an upgraded MiSTer?
+if [ -f "${INSTALLPATH}linux/_user-startup.sh" ] || [ -f "${INSTALLPATH}linux/user-startup.sh" ]; then
+      echo "${yellow}Updated MiSTer Detected, Good${white}"
+else
+      echo "${red}WARNING: Pixelcade may not be compatible with this version of MiSTer as no ${INSTALLPATH}linux/_user-startup.sh was found. You may need to upgrade to the latest MiSTer${white}"
+fi
+
+#is Pixelcade already installed?
+if [[ -d "${INSTALLPATH}pixelcade" ]]; then
+    echo "${yellow}Pixelcade is already installed, updating to the latest including artwork${white}"
+fi
+
+# let's detect if Pixelcade is connected
+pixelcade_led_detected=false
+if ls /dev/ttyACM0 | grep -q '/dev/ttyACM0'; then
+   echo "${yellow}Pixelcade LED Marquee Detected on /dev/ttyACM0${white}"
+   pixelcade_led_detected=true
+elif ls /dev/ttyACM1 | grep -q '/dev/ttyACM1'; then
+  echo "${yellow}Pixelcade LED Marquee Detected on /dev/ttyACM1${white}"
+  pixelcade_led_detected=true
+else
+   echo "${red}Pixelcade LED Marquee was not detected, pleasse ensure Pixelcade is USB connected to your MiSTer if you have a Pixelcade LED Marquee. You can still use Pixelcade with just a Pixelcade LCD Marquee connected to your network${white}"
+fi
+
+# TO DO ask user if they want to install on SD card or USB
+if [[ -d "${INSTALLPATH}" ]]; then
+  echo "${yellow}MiSTer SD Card Found${white}"
+else
+   echo "${yellow}MiSTer SD Card Not Found, Sorry setup cannot continue..."
+   exit 1
+fi
+
+if uname -m | grep -q 'armv6'; then
+   echo "${yellow}arm_v6 Detected..."
+   machine_arch=arm_v6
+fi
+
+if uname -m | grep -q 'armv7'; then
+   echo "${yellow}arm_v7 Detected..."
+   machine_arch=arm_v7
+fi
+
+if uname -m | grep -q 'aarch32'; then
+   echo "${yellow}aarch32 Detected..."
+   aarch32=arm_v7
+fi
+
+if uname -m | grep -q 'aarch64'; then
+   echo "${yellow}aarch64 Detected..."
+   machine_arch=arm64
+fi
+
+if uname -m | grep -q 'x86'; then
+   echo "${yellow}x86 32-bit Detected..."
+   machine_arch=386
+fi
+
+if uname -m | grep -q 'amd64'; then
+   echo "${yellow}x86 64-bit Detected..."
+   machine_arch=amd64
+fi
+
+if uname -m | grep -q 'x86_64'; then
+   echo "${yellow}x86 64-bit Detected..."
+   machine_arch=amd64
+fi
+
+cd ${INSTALLPATH}
+
+if [[ ! -d "${INSTALLPATH}pixelcade" ]]; then #create the pixelcade folder if it's not there
+   mkdir ${INSTALLPATH}pixelcade
+fi
+
+cd ${INSTALLPATH}pixelcade
+echo "Installing Pixelcade Software..."
+
+if [[ "$beta" == "true" ]]; then
+    url="https://github.com/alinke/pixelcade-linux-builds/raw/main/beta/linux_${machine_arch}/pixelweb"
+    if wget --spider "$url" 2>/dev/null; then
+        echo "${yellow}[BETA] A Pixelcade LED beta version is available so let's get it...${white}"
+        wget -O ${INSTALLPATH}pixelcade/pixelweb "$url"
+    else
+        echo "${yellow}There is no beta available at this time so we'll go with the production version${white}"
+        prod_url="https://github.com/alinke/pixelcade-linux-builds/raw/main/linux_${machine_arch}/pixelweb"
+        wget -O ${INSTALLPATH}pixelcade/pixelweb "$prod_url"
+    fi
+else
+    prod_url="https://github.com/alinke/pixelcade-linux-builds/raw/main/linux_${machine_arch}/pixelweb"
+    wget -O ${INSTALLPATH}pixelcade/pixelweb "$prod_url"
+fi
+
+chmod +x pixelweb
+./pixelweb -install-artwork #install the artwork
+
+if [[ $? == 2 ]]; then #this means artwork is already installed so let's check for updates and get if so
+  echo "Checking for new Pixelcade artwork..."
+  cd ${INSTALLPATH}pixelcade && ./pixelweb -update-artwork
+fi
+
+echo "${yellow}Downloading Additional Files Needed for Pixelcade MiSTer...${white}"
+cd ${INSTALLPATH}pixelcade
+
+wget -O ${INSTALLPATH}pixelcade/ip.txt https://raw.githubusercontent.com/alinke/pixelcade-linux-builds/main/mister/ip.txt
+wget -O ${INSTALLPATH}pixelcade/pixelcadeLink.sh https://raw.githubusercontent.com/alinke/pixelcade-linux-builds/main/mister/pixelcadeLink.sh
+wget -O ${INSTALLPATH}pixelcade/runpixelcade.sh https://raw.githubusercontent.com/alinke/pixelcade-linux-builds/main/mister/runpixelcade.sh
+chmod +x ${INSTALLPATH}pixelcade/runpixelcade.sh
+
+echo "${yellow}Skipping Pixelcade auto-start setup (Manual Start Mode) - Pixelcade will NOT be added to user-startup.sh${white}"
+echo "${yellow}You will start Pixelcade manually over SSH instead - see instructions at the end of this installer${white}"
+
+# If Pixelcade was already added to user-startup.sh from a previous normal install, remove it
+# so it doesn't fight with manual startup.
+if [[ -f "${INSTALLPATH}linux/user-startup.sh" ]] && grep -q 'runpixelcade' "${INSTALLPATH}linux/user-startup.sh"; then
+    echo "${yellow}Removing existing Pixelcade auto-start line from user-startup.sh...${white}"
+    sed -i "\#cd ${INSTALLPATH}pixelcade && ./runpixelcade.sh#d" "${INSTALLPATH}linux/user-startup.sh"
+fi
+
+
+echo "${yellow}Modifying MiSTer.ini to turn on current game logging which is needed for Pixelcade...${white}"
+cd ${INSTALLPATH}
+if [[ -f "${INSTALLPATH}MiSTer.ini" ]]; then
+    echo "${yellow}Updating your existing MiSTer.ini${white}"
+    add_log_entry_if_needed
+elif [[ -f "${INSTALLPATH}MiSTer_example.ini" ]]; then
+    echo "${yellow}Adding MiSTer.ini${white}"
+    mv ${INSTALLPATH}MiSTer_example.ini ${INSTALLPATH}MiSTer.ini
+    add_log_entry_if_needed
+    exit 1
+else
+    echo "${yellow}Getting a vanilla MiSTer.ini${white}"
+    wget -O ${INSTALLPATH}MiSTer.ini https://raw.githubusercontent.com/alinke/pixelcade-linux-builds/main/mister/MiSTer.ini
+    add_log_entry_if_needed
+fi
+
+echo ""
+pixelcade_version="$(cd ${INSTALLPATH}pixelcade && ./pixelweb -version)"
+echo "[INFO] $pixelcade_version Installed"
+if [[ "$beta" == "true" ]]; then
+    echo "[INFO] Beta version installed"
+fi
+install_succesful=true
+
+echo "Cleaning up..."
+if [[ -f ${INSTALLPATH}setup-mister.sh ]]; then
+    rm ${INSTALLPATH}setup-mister.sh.sh
+fi
+
+if [[ -f /storage/setup-mister.sh.sh ]]; then
+    rm /storage/setup-mister.sh.sh
+fi
+
+# Manual Start Mode: we do NOT auto-run Pixelcade here. See instructions at the end
+# of this installer for how to start it manually over SSH.
+
+# Check if LED was not detected and prompt for LCD setup
+if [ "$pixelcade_led_detected" = false ] ; then
+    while true; do
+        read -p "${yellow}Do you have a Pixelcade LCD Marquee and if so, is it connected to your WiFi or Ethernet? Note: MiSTer unfortunately does not support Pixelcade LCD over USB. (y/n)${white}" yn
+        case $yn in
+            [Yy]* ) 
+                echo "${yellow}Setting up Pixelcade LCD Marquee...${white}"
+                read -p "${yellow}Please enter your Pixelcade LCD's IP address: ${white}" lcd_ip_address
+                
+                # Update pixelcade.ini with LCD settings
+                cd ${INSTALLPATH}pixelcade
+                if [[ -f "pixelcade.ini" ]]; then
+                    echo "${yellow}Updating pixelcade.ini for LCD Marquee...${white}"
+                    # Update existing values only - using precise patterns to avoid duplicates
+                    sed -i "s/^lcdMarquee[[:space:]]*=.*/lcdMarquee = true/" pixelcade.ini
+                    sed -i "s/^lcdUsbConnected[[:space:]]*=.*/lcdUsbConnected = false/" pixelcade.ini
+                    sed -i "s/^lcdMarqueeIPAddress[[:space:]]*=.*/lcdMarqueeIPAddress = ${lcd_ip_address}/" pixelcade.ini
+                    sed -i "s/^lcdMarqueeHostName[[:space:]]*=.*/lcdMarqueeHostName = /" pixelcade.ini
+                    sed -i "s/^lcdDisableUSBCheck[[:space:]]*=.*/lcdDisableUSBCheck = true/" pixelcade.ini
+                    
+                    echo "${yellow}LCD Marquee configured with IP address: ${lcd_ip_address}${white}"
+                else
+                    echo "${red}Warning: pixelcade.ini not found. LCD settings could not be configured.${white}"
+                fi
+                break;;
+            [Nn]* ) 
+                echo "${yellow}Continuing without LCD Marquee setup...${white}"
+                break;;
+            * ) 
+                echo "Please answer yes or no.";;
+        esac
+    done
+fi
+
+echo ""
+echo "${magenta}=========================================================${white}"
+echo "${magenta}   INSTALLATION COMPLETE - MANUAL START MODE${white}"
+echo "${magenta}=========================================================${white}"
+echo ""
+echo "${yellow}Pixelcade has been installed but will NOT start automatically on boot.${white}"
+echo "${yellow}To start it manually, SSH into your MiSTer and run it yourself:${white}"
+echo ""
+echo "  1. From another computer, SSH into your MiSTer:"
+echo "       ${green}ssh root@<your-mister-ip>${white}"
+echo "     (default password is 1, unless you've changed it)"
+echo ""
+echo "  2. Go to the Pixelcade install folder:"
+echo "       ${green}cd ${INSTALLPATH}pixelcade${white}"
+echo ""
+echo "  3. Run Pixelcade in the foreground so you can see any errors/crash output:"
+echo "       ${green}./runpixelcade.sh${white}"
+echo "     (or run ${green}./pixelweb${white} directly for even more verbose output)"
+echo ""
+echo "  4. Leave that SSH session open while you test - Pixelcade will keep running"
+echo "     until you close the session or press Ctrl+C."
+echo ""
+echo "${yellow}Since it's not in user-startup.sh, a reboot will NOT relaunch Pixelcade -${white}"
+echo "${yellow}you'll need to SSH back in and run ./runpixelcade.sh again each time.${white}"
+echo ""
+
+while true; do
+    read -p "${magenta}Reboot Now? (y/n)${white}" yn
+    case $yn in
+        [Yy]* ) sudo reboot; break;;
+        [Nn]* ) echo "${yellow}Please reboot when you get a chance, then SSH in and run ./runpixelcade.sh manually as shown above${white}" && exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
